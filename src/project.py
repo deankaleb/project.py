0e149d1d0913a2ce203126e0c5c6ac58b39a1d49
deankaleb <deankal36@gmail.com>
2023-11-14 21:46:32 -0600

    Got my first firework

--- src/project.py
+++ src/project.py
@@ -2,22 +2,26 @@ import random
 import pygame
 import math
 
-class Particle():
-    def __init__(self, pos=(0, 0), size=8, life=1000, color=pygame.Color(0, 255, 0)):
+class Particle:
+    def __init__(self, pos=(0, 0), size=5, life=1000, color=pygame.Color(0, 255, 0)):
         self.pos = pos
         self.size = size
         self.color = color
         self.age = 0
         self.life = life
         self.dead = False
-        self.alpha = 255
+        self.alpha = 0  # Start with zero alpha
         self.surface = self.update_surface()
 
     def update(self, dt):
         self.age += dt
         if self.age > self.life:
             self.dead = True
-        self.alpha = 255 * (1 - (self.age / self.life))
+        # Fading in and out effect based on particle's age
+        if self.age < self.life / 2:  # Fade in
+            self.alpha = int((self.age / (self.life / 2)) * 255)
+        else:  # Fade out
+            self.alpha = int(((self.life - self.age) / (self.life / 2)) * 255)
 
     def update_surface(self):
         surf = pygame.Surface((self.size * 2, self.size * 2), pygame.SRCALPHA)
@@ -34,24 +38,27 @@ class Particle():
         surface.blit(self.surface, self.pos)
 
 class ParticleTrail():
-    def __init__(self, origin, num_particles, size, life):
+    def __init__(self, origin, num_circles, particles_per_circle, max_size, life):
         self.origin = origin
-        self.num_particles = num_particles
-        self.size = size
+        self.num_circles = num_circles
+        self.particles_per_circle = particles_per_circle
+        self.max_size = max_size
         self.life = life
         self.particles = []
-        self._init_circle()
+        self._init_circles()
 
-    def _init_circle(self):
-        circle_radius = 100  # Modify this radius for spacing between particles
-        for i in range(self.num_particles):
-            angle = (2 * math.pi / self.num_particles) * i
-            x = self.origin[0] + circle_radius * math.cos(angle)
-            y = self.origin[1] + circle_radius * math.sin(angle)
-            pos = (int(x), int(y))
-            life = random.uniform(500, 3000)
-            particle = Particle(pos, size=self.size, life=life)
-            self.particles.append(particle)
+
+    def _init_circles(self):
+        for circle in range(1, self.num_circles + 1):
+            circle_radius = (circle / self.num_circles) * self.max_size
+            for i in range(self.particles_per_circle):
+                angle = (2 * math.pi / self.particles_per_circle) * i
+                x = self.origin[0] + circle_radius * math.cos(angle)
+                y = self.origin[1] + circle_radius * math.sin(angle)
+                pos = (int(x), int(y))
+                life = random.uniform(500, 3000)
+                particle = Particle(pos, size=5, life=life)
+                self.particles.append(particle)
 
     def update(self, dt):
         for particle in self.particles:
@@ -60,6 +67,7 @@ class ParticleTrail():
     def draw(self, surface):
         for particle in self.particles:
             particle.draw(surface)
+
 
 def main():
     pygame.init()
@@ -70,8 +78,7 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
-    particle_trail = ParticleTrail((400, 300), num_particles=20, size=10, life=1000)  # Circular origin at (400, 300)
-  # Circular origin at (400, 300)
+    particle_trail = ParticleTrail((400, 300), num_circles=5, particles_per_circle=20, max_size=50, life=1000)
     running = True
     while running:
         for event in pygame.event.get():
